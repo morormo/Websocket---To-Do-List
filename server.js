c9c5859a9423c5500fd8ce95ed44275bae77545a
Morormo <morormo@gmail.com>
2020-05-19 14:14:22 +0200

    ADD addTask and removeTask

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+let tasks = [];
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (newTask) => {
+    tasks.push(newTask);
+    socket.broadcast.emit('addTask', newTask);
+  });
+  socket.emit('removeTask', (idTask) => {
+    tasks.splice(idTask);
+    socket.broadcast.emit('removeTask', idTask);
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
